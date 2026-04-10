e8e8fed5db4fdb06541605b20f17e73cbc525b5b
fangrong <darkbluesky@outlook.com>
2026-04-10 12:16:48 +0800

    switch_usb: Remove multiple definition of enums for latest libnx

--- source/os/switch/switch_usb.cpp
+++ source/os/switch/switch_usb.cpp
@@ -27,20 +27,6 @@ enum usb_request_bot {
     USB_REQUEST_BOT_RESET       = 0xFF
 };
 
-enum usb_request_recipient {
-    USB_RECIPIENT_DEVICE    = 0x00,
-    USB_RECIPIENT_INTERFACE = 0x01,
-    USB_RECIPIENT_ENDPOINT  = 0x02,
-    USB_RECIPIENT_OTHER     = 0x03,
-};
-
-enum usb_request_type {
-    USB_REQUEST_TYPE_STANDARD = (0x00 << 5),
-    USB_REQUEST_TYPE_CLASS    = (0x01 << 5),
-    USB_REQUEST_TYPE_VENDOR   = (0x02 << 5),
-    USB_REQUEST_TYPE_RESERVED = (0x03 << 5),
-};
-
 CSWITCH_USB::~CSWITCH_USB(){
     
     usbHsEpClose(&endpoint_out);
